93b78f3a6dd2c0a4b8ef623ea3b03a5ff1d6a7c9
Panji Kumbara <108613621+PanjiKumbara@users.noreply.github.com>
2023-11-18 15:05:06 +0700

    Delete foodorder directory

--- foodorder/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$conn = mysqli_connect('localhost','root','','foodorder') or die('connection failed')
-
-
-
-?>
--- foodorder/profile.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Company Profile</title>
-</head>
-<body>
-  <img src="img/RelasiDatabase.img<?php echo $img; ?>" alt="RelasiDatabase.img">
-</body>
-</html>
